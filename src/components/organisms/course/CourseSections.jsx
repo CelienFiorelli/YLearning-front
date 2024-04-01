b78cad73ef380fb28b3d97ea27004ae35e9bab69
Brunel Geordi GAMPIO <80811860+Brunel-Geordi@users.noreply.github.com>
2024-04-02 00:42:49 +0200

    correction probleme de responses

--- src/components/organisms/course/CourseSections.jsx
+++ src/components/organisms/course/CourseSections.jsx
@@ -1,12 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
 import Base from "../Base";
-import {
-  getCourseSections,
-} from "../../../services/courseRequest";
+import { getCourseSections } from "../../../services/courseRequest";
 import { useParams } from "react-router";
 import { AuthContext } from "../AuthProvider";
 import SectionsCard from "../../molecules/course/SectionsCard";
 import ResponseSection from "../../molecules/course/Responses";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const CourseSections = () => {
   const { id } = useParams();
@@ -17,10 +17,16 @@ export const CourseSections = () => {
     if (!token) return;
     (async () => {
       const res = await getCourseSections(id, token);
-      console.log(res)
       setData(res);
     })();
   }, [token]);
+
+  const succes = () => {
+    toast.success(`Bonne réponse !`);
+  };
+  const missed = (username) => {
+    toast.error(`Mauvaise réponse !`);
+  };
 
   return (
     <Base>
@@ -30,19 +36,33 @@ export const CourseSections = () => {
         </div>
         <div className="w-3/4 border my-12 gap-4 mb-32">
           {data &&
-            data.sections.map((s, i) => {
-              return (
-                <div className="bg-[#606466]">
-                  <SectionsCard sections={s} index={i + 1} />
-                  <label htmlFor="" className="px-4 font-semibold text-md text-white"> Mise en pratique :</label>
-                  <div className="px-4 w-full grid grid-cols-2">
-                    {data.responses.map((r, i) => {
-                      return <ResponseSection responses={r} index={i + 1} />;
-                    })}
-                  </div>
+            data.map((sectionData, index) => (
+              <div className="bg-[#606466]" key={index}>
+                <SectionsCard
+                  sections={sectionData.section}
+                  index={index + 1}
+                />
+                <label
+                  htmlFor=""
+                  className="px-4 font-semibold text-md text-white"
+                >
+                  Mise en pratique :
+                </label>
+                <div className="px-4 w-full grid grid-cols-2">
+                  {sectionData.responses.map((response, i) => (
+                    <ResponseSection
+                      key={i}
+                      responses={response}
+                      index={i + 1}
+                      onClick={() => response.isValid ? succes() : missed()}
+                    />
+                  ))}
                 </div>
-              );
-            })}
+              </div>
+            ))}
+        </div>
+        <div className="absolute">
+          <ToastContainer />
         </div>
       </div>
     </Base>
